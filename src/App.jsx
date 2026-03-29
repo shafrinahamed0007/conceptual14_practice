c6fbadc6be6167bf8c94df7b77879245db83cb1f
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-29 23:35:32 +0600

    project setup successfull

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavBar from "./Components/Navbar";
+import Banner from "./Components/Banner";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Banner />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
